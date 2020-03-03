3db799c270ffd9ef34cd07e1488c33148c7bddc2
Tom Hughes <thomas-hughes@live.co.uk>
2020-03-03 21:53:27 +0000

    Runs elm-format on Bob solution

--- elm/bob/src/Bob.elm
+++ elm/bob/src/Bob.elm
@@ -1,20 +1,25 @@
-
 module Bob exposing (hey)
 
-import String exposing (toUpper, endsWith, trim, isEmpty, any)
+import String exposing (any, endsWith, isEmpty, toUpper, trim)
+
 
 hey : String -> String
 hey remark =
     if isEmpty <| trim <| remark then
         "Fine. Be that way!"
+
     else if toUpper remark == remark && endsWith "?" remark && hasLetters remark then
         "Calm down, I know what I'm doing!"
+
     else if toUpper remark == remark && hasLetters remark then
         "Whoa, chill out!"
+
     else if endsWith "?" (remark |> trim) then
         "Sure."
+
     else
         "Whatever."
+
 
 hasLetters : String -> Bool
 hasLetters remark =
