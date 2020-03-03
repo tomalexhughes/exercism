d602d6972e6e8fdc68d19a79be78cddb263f6598
Tom Hughes <thomas-hughes@live.co.uk>
2020-03-03 21:51:44 +0000

    Adds initial attempt at Elm Bob exercise

---
+++ elm/bob/src/Bob.elm
@@ -0,0 +1,21 @@
+
+module Bob exposing (hey)
+
+import String exposing (toUpper, endsWith, trim, isEmpty, any)
+
+hey : String -> String
+hey remark =
+    if isEmpty <| trim <| remark then
+        "Fine. Be that way!"
+    else if toUpper remark == remark && endsWith "?" remark && hasLetters remark then
+        "Calm down, I know what I'm doing!"
+    else if toUpper remark == remark && hasLetters remark then
+        "Whoa, chill out!"
+    else if endsWith "?" (remark |> trim) then
+        "Sure."
+    else
+        "Whatever."
+
+hasLetters : String -> Bool
+hasLetters remark =
+    any Char.isAlpha remark
