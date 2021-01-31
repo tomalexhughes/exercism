8f1e3f5faa4bed493e174b54a09b0c0d916d68e8
Tom Hughes <thomas-hughes@live.co.uk>
2021-01-31 17:29:24 +0000

    Elixir Clock exercises test now green

--- elixir/clock/lib/clock.ex
+++ elixir/clock/lib/clock.ex
@@ -8,7 +8,29 @@ defmodule Clock do
       "08:09"
   """
   @spec new(integer, integer) :: Clock
+
+  def new(hour, minute) when minute < 0 do
+    new(hour - 1, 60 + minute)
+  end
+
+  def new(hour, minute) when hour < 0 do
+    new(24 + hour, minute)
+  end
+
+  def new(hour, minute) when minute >= 60 do
+    new(hour + 1, minute - 60)
+  end
+
+  def new(hour, minute) when hour >= 24 do
+    new(hour - 24, minute)
+  end
+
+  def new(24, minute) do
+    new(0, minute)
+  end
+
   def new(hour, minute) do
+    %Clock{hour: hour, minute: minute}
   end
 
   @doc """
@@ -19,5 +41,13 @@ defmodule Clock do
   """
   @spec add(Clock, integer) :: Clock
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
+    new(hour, minute + add_minute)
+  end
+end
+
+defimpl String.Chars, for: Clock do
+  def to_string(clock) do
+    String.pad_leading(Integer.to_string(clock.hour), 2, "0") <>
+      ":" <> String.pad_leading(Integer.to_string(clock.minute), 2, "0")
   end
 end
