9cfe0bcce0ed889f08ffe4efcde9938f64b82ca1
Tom Hughes <thomas-hughes@live.co.uk>
2021-01-31 18:13:01 +0000

    Refactor of Elixir clock exercise

--- elixir/clock/lib/clock.ex
+++ elixir/clock/lib/clock.ex
@@ -8,30 +8,11 @@ defmodule Clock do
       "08:09"
   """
   @spec new(integer, integer) :: Clock
-
-  def new(hour, minute) when minute < 0 do
-    new(hour - 1, 60 + minute)
-  end
-
-  def new(hour, minute) when hour < 0 do
-    new(24 + hour, minute)
-  end
-
-  def new(hour, minute) when minute >= 60 do
-    new(hour + 1, minute - 60)
-  end
-
-  def new(hour, minute) when hour >= 24 do
-    new(hour - 24, minute)
-  end
-
-  def new(24, minute) do
-    new(0, minute)
-  end
-
-  def new(hour, minute) do
-    %Clock{hour: hour, minute: minute}
-  end
+  def new(hour, minute) when minute < 0, do: new(hour - 1, 60 + minute)
+  def new(hour, minute) when hour < 0, do: new(24 + hour, minute)
+  def new(hour, minute) when minute >= 60, do: new(hour + 1, minute - 60)
+  def new(hour, minute) when hour >= 24, do: new(hour - 24, minute)
+  def new(hour, minute), do: %Clock{hour: hour, minute: minute}
 
   @doc """
   Adds two clock times:
@@ -40,14 +21,12 @@ defmodule Clock do
       "10:03"
   """
   @spec add(Clock, integer) :: Clock
-  def add(%Clock{hour: hour, minute: minute}, add_minute) do
-    new(hour, minute + add_minute)
-  end
+  def add(%Clock{hour: hour, minute: minute}, add_minute), do: new(hour, minute + add_minute)
 end
 
 defimpl String.Chars, for: Clock do
-  def to_string(clock) do
-    String.pad_leading(Integer.to_string(clock.hour), 2, "0") <>
-      ":" <> String.pad_leading(Integer.to_string(clock.minute), 2, "0")
+  def to_string(%Clock{hour: hour, minute: minute}) do
+    pad = fn x -> String.pad_leading(Integer.to_string(x), 2, "0") end
+    pad.(hour) <> ":" <> pad.(minute)
   end
 end
