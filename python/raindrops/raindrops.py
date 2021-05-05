7a4b7b035ea457f915aff235a727922d5412f13b
Tom Hughes <tomalexhughes@users.noreply.github.com>
2021-05-05 21:39:06 +0100

    Completes raindrops.py

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,14 @@
 def convert(number):
-    pass
+    output = ""
+
+    if (number % 3 == 0):
+        output += "Pling"
+    if (number % 5 == 0):
+        output += "Plang"
+    if (number % 7 == 0):
+        output += "Plong"
+
+    if (output == ""):
+        return str(number)
+
+    return output
