1e1da754220acdb57a0d652aefc786b05d612db8
Tom Hughes <tomalexhughes@users.noreply.github.com>
2021-09-27 21:41:30 +0100

    Initial solution to Elixir darts.ex exercise
    This was a bit more of a maths problem than I'd like
    Formula from https://stackoverflow.com/a/481150

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,18 @@ defmodule Darts do
   """
   @spec score(position :: position) :: integer
   def score({x, y}) do
+    cond do
+      is_within_inner_circle({x, y}) -> 10
+      is_within_middle_circle({x, y}) -> 5
+      is_within_outer_circle({x, y}) -> 1
+      true -> 0
+    end
   end
+
+  defp is_within_circle({x, y}, radius),
+    do: :math.pow(x, 2) + :math.pow(y, 2) <= :math.pow(radius, 2)
+
+  defp is_within_outer_circle({x, y}), do: is_within_circle({x, y}, 10)
+  defp is_within_middle_circle({x, y}), do: is_within_circle({x, y}, 5)
+  defp is_within_inner_circle({x, y}), do: is_within_circle({x, y}, 1)
 end
