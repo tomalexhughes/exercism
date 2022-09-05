0bc202f8276e8c9265f912b57e51c149d97ab4bd
Tom Hughes <tom+git@tah.dev>
2022-09-05 22:10:00 +0100

    completes Elixir guessing game exercise

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,10 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
-  end
+  # Bodyless function - see https://groups.google.com/g/elixir-lang-talk/c/JCIGPvppvDA
+  def compare(secret_number, guess \\ :no_guess)
+
+  def compare(secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when guess == secret_number, do: "Correct"
+  def compare(secret_number, guess) when abs(guess - secret_number) == 1, do: "So close"
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
 end
