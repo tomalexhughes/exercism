7e2b68f2d3e1b44eeeb6f12f70407d61364f8e7b
Tom Hughes <tom+git@tah.dev>
2022-09-05 21:39:52 +0100

    completes elixir language-list exercise

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,26 @@
 defmodule LanguageList do
   def new() do
-    # Please implement the new/0 function
+    []
   end
 
   def add(list, language) do
-    # Please implement the add/2 function
+    [language | list]
   end
 
   def remove(list) do
-    # Please implement the remove/1 function
+    [_first | rest] = list
+    rest
   end
 
   def first(list) do
-    # Please implement the first/1 function
+    Enum.at(list, 0)
   end
 
   def count(list) do
-    # Please implement the count/1 function
+    Enum.count(list)
   end
 
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
+    Enum.member?(list, "Elixir")
   end
 end
