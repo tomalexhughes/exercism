5c6a19b0c8420511a755926f0d60c71121ef2b87
Tom Hughes <tom+git@tah.dev>
2023-02-08 21:47:34 +0000

    Downloads Elm "Betty's Bike Shop" exercise

---
+++ elm/bettys-bike-shop/src/BettysBikeShop.elm
@@ -0,0 +1,11 @@
+module BettysBikeShop exposing (poundsToString)
+
+-- TODO: import the String module
+
+
+penceToPounds pence =
+    Debug.todo "implement this function and create a type annotation"
+
+
+poundsToString pounds =
+    Debug.todo "implement this function and create a type annotation"
