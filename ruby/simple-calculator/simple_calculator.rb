923437f17667149b5677eef41f94efd72ec8f26d
Tom Hughes <tom+git@tah.dev>
2023-03-27 20:10:13 +0100

    Completes Ruby 'Simple Calculator' exercise

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -2,6 +2,15 @@ class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    raise(ArgumentError, 'numbers only please') if !first_operand.is_a?(Integer) || !second_operand.is_a?(Integer)
+    raise UnsupportedOperation unless ["+", "*", "/"].include?(operation)
+
+    result = first_operand.public_send(operation, second_operand)
+    "#{first_operand} #{operation} #{second_operand} = #{result}"
+  rescue ZeroDivisionError
+    "Division by zero is not allowed."
+  end
+
+  class UnsupportedOperation < StandardError
   end
 end
