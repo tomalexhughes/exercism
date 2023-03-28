083201b23ed6271774c2564aba8f278fc77d571d
Tom Hughes <tom+git@tah.dev>
2023-03-28 21:38:20 +0100

    Completes Ruby 'Assembly Line' exercise

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,24 @@
 class AssemblyLine
+
+  BASE_HOURLY_RATE = 221
+
+  SUCCESS_RATE_TABLE = {
+    (1..4) => 1,
+    (5..8) => 0.9,
+    (9..9) => 0.8,
+    (10..10) => 0.77
+  }
+
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    success_rate = SUCCESS_RATE_TABLE.detect{ |k, v| k.include?(@speed) }.last
+    @speed * BASE_HOURLY_RATE * success_rate
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).floor
   end
 end
