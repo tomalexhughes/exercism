a2f2156a799112e45980fcf9d05edb83b0c95132
Tom Hughes <tom+git@tah.dev>
2023-06-11 18:41:06 +0100

    Completes Python "Guido's Gorgeous Lasagna" exercise

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -7,12 +7,11 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME_PER_LAYER = 2
 
-#TODO: define the 'EXPECTED_BAKE_TIME' constant.
 
-
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+def bake_time_remaining(minutes_in_oven):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -22,14 +21,30 @@ def bake_time_remaining():
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
-
-    pass
-
-
-#TODO: Define the 'preparation_time_in_minutes()' function below.
-# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
+    return EXPECTED_BAKE_TIME - minutes_in_oven
 
 
+def preparation_time_in_minutes(number_of_layers):
+    """Calculate how many minutes a lasanga will take to prepare.
 
-#TODO: define the 'elapsed_time_in_minutes()' function below.
-# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
+    :param number_of_layers: int - number of layers of the lasagna.
+    :return: int - total time spent (in minutes) to prepare the lasagna
+
+    Function that takes the number of lasagna layers and returns how many
+    minutes are spent to prepare the lasagna.
+    """
+    return PREPARATION_TIME_PER_LAYER * number_of_layers
+
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate the elapsed time in minutes.
+
+    :param number_of_layers: int - number of layers of the lasagna.
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - total time spent (in minutes) including preparation & baking
+
+    Function that takes the number of lasagna layers and the time in the oven
+    returns how many minutes has been spent in total preparing and baking the
+    lasagna.
+    """
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
