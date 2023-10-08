7f46911ef210a318161903dfc4a56928437b9289
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2023-10-08 10:39:46 +0100

    Updates `gigasecond` to be a private variable in Kotlin exercise as per feedback

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 class Gigasecond(localDateTime: LocalDateTime) {
     constructor(localDate: LocalDate) : this(localDate.atStartOfDay())
 
-    val gigasecond: Long = 1_000_000_000
+    private val gigasecond: Long = 1_000_000_000
 
     val date: LocalDateTime = localDateTime.plusSeconds(gigasecond)
 }
