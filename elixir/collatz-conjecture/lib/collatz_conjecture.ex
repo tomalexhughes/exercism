ec25640568068b0746f191334aef6c99ba05b813
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-02-17 16:05:27 +0000

    Completes Elixir 'Collatz Conjecture' exercise

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -1,11 +1,16 @@
 defmodule CollatzConjecture do
+  require Integer
+
   @doc """
   calc/1 takes an integer and returns the number of steps required to get the
   number to 1 when following the rules:
     - if number is odd, multiply with 3 and add 1
     - if number is even, divide by 2
   """
-  @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) do
-  end
+  @spec calc(input :: pos_integer(), count :: non_neg_integer()) :: non_neg_integer()
+  def calc(input, count \\ 0)
+  def calc(input, _count) when is_binary(input) or input <= 0, do: raise(FunctionClauseError)
+  def calc(1, count), do: count
+  def calc(input, count) when Integer.is_even(input), do: calc(div(input, 2), count + 1)
+  def calc(input, count) when Integer.is_odd(input), do: calc(input * 3 + 1, count + 1)
 end
