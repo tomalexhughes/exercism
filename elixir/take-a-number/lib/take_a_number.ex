6d173c695871e0047931a0bf5aa9fa52d93c9079
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-02-19 22:48:51 +0000

    Completes 'Take a Number' exercise in Elixir

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,27 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(&loop/0)
+  end
+
+  defp loop(number \\ 0) do
+    # If there are no messages in the mailbox,
+    # or none of messages in the mailbox match any of the patterns,
+    # receive/1 is going to wait for one.
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, number)
+        loop(number)
+
+      {:take_a_number, sender_pid} ->
+        send(sender_pid, number + 1)
+        loop(number + 1)
+
+      :stop ->
+        nil
+
+      # Keep waiting for messages after receiving an unexpected message
+      _ ->
+        loop(number)
+    end
   end
 end
