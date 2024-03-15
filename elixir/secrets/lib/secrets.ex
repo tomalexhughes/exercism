9f759350ccee4d1b43566bea1857079e0988bf4a
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-03-15 21:03:12 +0000

    Alternative solution to Elixir 'Secrets' exercise

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,12 +1,14 @@
 defmodule Secrets do
-  def secret_add(secret), do: &(&1 + secret)
-  def secret_subtract(secret), do: &(&1 - secret)
-  def secret_multiply(secret), do: &(&1 * secret)
+  def secret_add(secret), do: &Kernel.+(&1, secret)
+  def secret_subtract(secret), do: &Kernel.-(&1, secret)
+  def secret_multiply(secret), do: &Kernel.*(&1, secret)
   # NOTE: div/2 used for integer division, instead of / which returns a float
-  def secret_divide(secret), do: &div(&1, secret)
+  def secret_divide(secret), do: &Kernel.div(&1, secret)
   def secret_and(secret), do: &Bitwise.band(&1, secret)
   def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
 
   def secret_combine(secret_function1, secret_function2),
-    do: &secret_function2.(secret_function1.(&1))
+    # NOTE: secret_function1 and secret_function2 are anonymous functions
+    # (returned via the above helpers). Hence needing the `.`
+    do: &(&1 |> secret_function1.() |> secret_function2.())
 end
