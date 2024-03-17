3fe935fe5aed1d7c83dedc0de41cf37d4d4b1bb7
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-03-17 12:37:32 +0000

    Refactor Elixir 'Knapsack' exercise to reduce arguments on recursion

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -4,18 +4,14 @@ defmodule Knapsack do
   """
   @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
           integer
-  def maximum_value(items, maximum_weight, bag_value \\ 0)
-  def maximum_value([], _maximum_weight, bag_value), do: bag_value
+  def maximum_value([], _), do: 0
 
-  def maximum_value([hd | tl], maximum_weight, bag_value) when hd.weight > maximum_weight,
-    do: maximum_value(tl, maximum_weight, bag_value)
+  def maximum_value([hd | tl], maximum_weight) when hd.weight > maximum_weight,
+    do: maximum_value(tl, maximum_weight)
 
-  def maximum_value([hd | tl], maximum_weight, bag_value) do
-    value_with_item =
-      maximum_value(tl, maximum_weight - hd[:weight], bag_value + hd[:value])
-
-    value_without_item = maximum_value(tl, maximum_weight, bag_value)
-
+  def maximum_value([hd | tl], maximum_weight) do
+    value_with_item = hd.value + maximum_value(tl, maximum_weight - hd.weight)
+    value_without_item = maximum_value(tl, maximum_weight)
     max(value_with_item, value_without_item)
   end
 end
