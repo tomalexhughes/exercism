f8fc42e4fe7b5cb405aafe1900b90ab5558d038e
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-03-17 12:17:36 +0000

    refactor: remove `cond` from Elixir 'Knapsack' exercise

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -7,22 +7,16 @@ defmodule Knapsack do
   def maximum_value(items, maximum_weight, bag_value \\ 0)
   def maximum_value([], _maximum_weight, bag_value), do: bag_value
 
+  def maximum_value([%{weight: weight} | tail], maximum_weight, bag_value)
+      when weight > maximum_weight,
+      do: maximum_value(tail, maximum_weight, bag_value)
+
   def maximum_value([head | tail], maximum_weight, bag_value) do
-    cond do
-      !is_item_under_weight_limit?(head, maximum_weight) ->
-        maximum_value(tail, maximum_weight, bag_value)
+    value_with_item =
+      maximum_value(tail, maximum_weight - head[:weight], bag_value + head[:value])
 
-      true ->
-        value_with_item =
-          maximum_value(tail, maximum_weight - head[:weight], bag_value + head[:value])
+    value_without_item = maximum_value(tail, maximum_weight, bag_value)
 
-        value_without_item = maximum_value(tail, maximum_weight, bag_value)
-
-        max(value_with_item, value_without_item)
-    end
-  end
-
-  defp is_item_under_weight_limit?(item, maximum_weight) do
-    item[:weight] <= maximum_weight
+    max(value_with_item, value_without_item)
   end
 end
