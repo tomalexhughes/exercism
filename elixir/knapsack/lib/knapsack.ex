3d1b612f9a0f4c564fd4dd3e4a0afae879d2b9be
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-03-17 12:06:46 +0000

    refactor: remove code in Elixir 'Knapsack' exercise

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -6,13 +6,9 @@ defmodule Knapsack do
           integer
   def maximum_value(items, maximum_weight, bag_value \\ 0)
   def maximum_value([], _maximum_weight, bag_value), do: bag_value
-  def maximum_value(_list, 0, bag_value), do: bag_value
 
   def maximum_value([head | tail], maximum_weight, bag_value) do
     cond do
-      !has_item_under_weight_limit?([head | tail], maximum_weight) ->
-        maximum_value([], maximum_weight, bag_value)
-
       !is_item_under_weight_limit?(head, maximum_weight) ->
         maximum_value(tail, maximum_weight, bag_value)
 
@@ -28,9 +24,5 @@ defmodule Knapsack do
 
   defp is_item_under_weight_limit?(item, maximum_weight) do
     item[:weight] <= maximum_weight
-  end
-
-  defp has_item_under_weight_limit?(items, maximum_weight) do
-    Enum.any?(items, &is_item_under_weight_limit?(&1, maximum_weight))
   end
 end
