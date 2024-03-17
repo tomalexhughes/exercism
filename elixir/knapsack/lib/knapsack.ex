a11fb65ae630cc7a41cddb9fca73d861c8321e89
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-03-17 12:25:54 +0000

    refactor: rename list variables to `hd` and `tl` in Elixir 'Knapsack' exercise

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -7,15 +7,15 @@ defmodule Knapsack do
   def maximum_value(items, maximum_weight, bag_value \\ 0)
   def maximum_value([], _maximum_weight, bag_value), do: bag_value
 
-  def maximum_value([%{weight: weight} | tail], maximum_weight, bag_value)
+  def maximum_value([%{weight: weight} | tl], maximum_weight, bag_value)
       when weight > maximum_weight,
-      do: maximum_value(tail, maximum_weight, bag_value)
+      do: maximum_value(tl, maximum_weight, bag_value)
 
-  def maximum_value([head | tail], maximum_weight, bag_value) do
+  def maximum_value([hd | tl], maximum_weight, bag_value) do
     value_with_item =
-      maximum_value(tail, maximum_weight - head[:weight], bag_value + head[:value])
+      maximum_value(tl, maximum_weight - hd[:weight], bag_value + hd[:value])
 
-    value_without_item = maximum_value(tail, maximum_weight, bag_value)
+    value_without_item = maximum_value(tl, maximum_weight, bag_value)
 
     max(value_with_item, value_without_item)
   end
