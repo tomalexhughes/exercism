e4d485fa5f37a7334e0a08d4173aac3a0831f525
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-03-24 18:03:51 +0000

    Working Binary Search algorithm in Elixir

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -18,6 +18,23 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
+  def search(numbers, item, index \\ 0)
+  def search({item}, item, index), do: {:ok, index}
+  def search({}, _, _), do: :not_found
+  def search({tuple_item}, item, _) when tuple_item != item, do: :not_found
+
+  def search(numbers, key, index) do
+    size = tuple_size(numbers)
+    as_list = Tuple.to_list(numbers)
+    middle_index = size |> div(2)
+    middle_item = as_list |> Enum.at(middle_index)
+    is_before_split = middle_item > key
+    range = if is_before_split, do: 0..(middle_index - 1), else: middle_index..-1//1
+    sliced = as_list |> Enum.slice(range)
+
+    number_of_elements_removed =
+      if !is_before_split, do: length(as_list) - length(sliced), else: 0
+
+    search(List.to_tuple(sliced), key, index + number_of_elements_removed)
   end
 end
