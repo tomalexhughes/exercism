21826e1238e616fffed14405fa28a2da38bb990b
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-04-21 14:56:36 +0100

    Completes Ruby 'BlackJack' exercise
    This was done using Vim, so the code quality may have a taken a hit
    whilst I focussed on learning Vim.

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,13 +1,40 @@
 module Blackjack
+  CARDS = { 'joker' => 0, 'ace' => 11, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8,
+            'nine' => 9, 'ten' => 10, 'jack' => 10, 'queen' => 10, 'king' => 10 }
+
   def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+    CARDS[card]
   end
 
   def self.card_range(card1, card2)
-    raise "Please implement the Blackjack.card_range method"
+    score = parse_card(card1) + parse_card(card2)
+
+    case score
+    when 4..11
+      'low'
+    when 12..16
+      'mid'
+    when 17..20
+      'high'
+    else
+      'blackjack'
+    end
   end
 
   def self.first_turn(card1, card2, dealer_card)
-    raise "Please implement the Blackjack.first_turn method"
+    score = parse_card(card1) + parse_card(card2)
+
+    case score
+    when 22
+      'P'
+    when 21
+      [10, 11].include?(parse_card(dealer_card)) ? 'S' : 'W'
+    when 17..20
+      'S'
+    when 12..16
+      parse_card(dealer_card) >= 7 ? 'H' : 'S'
+    else
+      'H'
+    end
   end
 end
