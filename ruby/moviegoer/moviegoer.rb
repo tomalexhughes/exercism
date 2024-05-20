222f62cfddd9d94084d304919653f89d2ea8b2cc
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-05-20 23:19:18 +0100

    Completes Ruby 'Moviegoer' exercise

--- ruby/moviegoer/moviegoer.rb
+++ ruby/moviegoer/moviegoer.rb
@@ -9,15 +9,15 @@ class Moviegoer
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    @age >= 60 ? 10 : 15
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    @age >= 18
   end
 
   # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    @member ? '🍿' : (raise NotMovieClubMemberError)
   end
 end
