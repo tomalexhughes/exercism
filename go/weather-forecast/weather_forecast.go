0d37d1748306e0a88966ef63a17acf603ba41d18
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-08-17 23:47:48 +0100

    Completes "Weather Forecast" in Go

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,15 @@
+// Package weather provides a function to forecast the current weather
+// conditions of a city in Goblinocus.
 package weather
 
+// CurrentCondition is used to store the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is used to store the city being forecasted.
 var CurrentLocation string
 
+// Forecast returns a string for the forecast of a given city and weather
+// condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
