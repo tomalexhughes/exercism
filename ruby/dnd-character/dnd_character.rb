96264df7ad47fb53df78fe7c759e6395a6eb3a16
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-08-31 10:56:36 +0100

    Completes "D&D Character" in Ruby

--- ruby/dnd-character/dnd_character.rb
+++ ruby/dnd-character/dnd_character.rb
@@ -1,17 +1,27 @@
-=begin
-Write your code for the 'D&D Character' exercise in this file. Make the tests in
-`dnd_character_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/dnd-character` directory.
-=end
-
 class DndCharacter
-  def self.modifier
-    # Your code here
+  attr_reader :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :hitpoints
+
+  def self.modifier(constitution)
+    ((constitution - 10) / 2).to_i
   end
 
   def initialize
-    # Your code here
+    @strength = roll
+    @dexterity = roll
+    @constitution = roll
+    @intelligence = roll
+    @wisdom = roll
+    @charisma = roll
+    @hitpoints = 10 + DndCharacter.modifier(@constitution)
+  end
+
+  private
+
+  def roll
+    Array.new(4) { rand(1..6) }
+         .sort
+         .reverse
+         .take(3)
+         .sum
   end
 end
