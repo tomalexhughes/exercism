ac70720e49f1f717256e1a054c50d50d701c9c1f
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-09-07 16:47:08 +0100

    Completes "Isogram" exercise in Ruby

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,7 +1,6 @@
-=begin
-Write your code for the 'Isogram' exercise in this file. Make the tests in
-`isogram_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/isogram` directory.
-=end
+class Isogram
+  def self.isogram?(input)
+    letters = input.downcase.gsub(/[^a-z]/, '')
+    letters.chars.length == letters.chars.uniq.length
+  end
+end
