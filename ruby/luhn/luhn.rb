bdb66fa990bf8dd4f3ba1d36eb1ece167b0b5aaf
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-09-11 20:07:33 +0100

    Complete "Luhn" in Ruby

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,7 +1,18 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+class Luhn
+  def self.valid?(input)
+    input = input.gsub(' ', '')
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
+    return false if /\D/.match(input)
+    return false if input.length <= 1
+
+    input
+      .chars
+      .reverse
+      .map.with_index { |d, i| i.odd? ? d.to_i * 2 : d.to_i }
+      .map { |d| d > 9 ? d - 9 : d } # We know if it's > 9 it's the result of our doubling
+      .sum
+      .modulo(10)
+      .zero?
+  end
+end
+
