1339a91c89973d46366ffc2d1fc968f6e63c44a4
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-10-10 23:03:14 +0100

    Completes "Locomotive Engineer" in Ruby

--- ruby/locomotive-engineer/locomotive_engineer.rb
+++ ruby/locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,17 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+  def self.generate_list_of_wagons(*wagon_ids)
+    wagon_ids
   end
 
-  def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+  def self.fix_list_of_wagons((wagon_1, wagon_2, wagon_3, *remaining_wagons), missing_wagons)
+    [wagon_3, *missing_wagons, *remaining_wagons, wagon_1, wagon_2]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(routing_hash, **stops)
+    { **routing_hash, stops: stops.values }
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    { **route, **more_route_information }
   end
 end
