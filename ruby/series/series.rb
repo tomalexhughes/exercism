fdd786a7f93d77e3c44e0810d869d38534e808b9
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-10-24 23:40:33 +0100

    Complete "Series" exercise in Ruby

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,7 +1,15 @@
-=begin
-Write your code for the 'Series' exercise in this file. Make the tests in
-`series_test.rb` pass.
+class Series
+  def initialize(digits_string)
+    @digits_string = digits_string
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/series` directory.
-=end
+  def slices(size)
+    raise ArgumentError if size > @digits_string.length
+
+    @digits_string
+      .chars
+      .each_cons(size)
+      .to_a
+      .map(&:join)
+  end
+end
