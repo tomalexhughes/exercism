42fcafe5f3e5a54847932bc47afbb94310a320ea
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2024-10-31 21:45:20 +0000

    Complete "Clock" exercise in Ruby

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,7 +1,28 @@
-=begin
-Write your code for the 'Clock' exercise in this file. Make the tests in
-`clock_test.rb` pass.
+class Clock
+  attr_reader :hours, :minutes
 
-To get started with TDD, see the `README.md` file in your
-`ruby/clock` directory.
-=end
+  def initialize(hour: 0, minute: 0)
+    @minutes = minute % 60
+    @hours = (hour + minute.div(60)) % 24
+  end
+
+  def to_s
+    format('%02d:%02d', hours, minutes)
+  end
+
+  def +(other)
+    Clock.new(hour: hours + other.hours, minute: minutes + other.minutes)
+  end
+
+  def -(other)
+    Clock.new(hour: hours - other.hours, minute: minutes - other.minutes)
+  end
+
+  def ==(other)
+    hours == other.hours && minutes == other.minutes
+
+    # instance_variables.all? do |var|
+    #   instance_variable_get(var) == other.instance_variable_get(var)
+    # end
+  end
+end
