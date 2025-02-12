c5a01edcd4fe7aca488fdf3912045fd084ce411d
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2025-02-12 22:59:17 +0000

    Complete "Remote Control Car" in Elixir

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,23 +1,45 @@
 defmodule RemoteControlCar do
-  # Please implement the struct with the specified fields
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
   def new() do
-    # Please implement the new/0 function
+    %RemoteControlCar{
+      nickname: "none"
+    }
   end
 
   def new(nickname) do
-    # Please implement the new/1 function
+    %RemoteControlCar{
+      battery_percentage: 100,
+      distance_driven_in_meters: 0,
+      nickname: nickname
+    }
   end
 
-  def display_distance(remote_car) do
-    # Please implement the display_distance/1 function
+  def display_distance(%RemoteControlCar{distance_driven_in_meters: distance_driven_in_meters}) do
+    "#{distance_driven_in_meters} meters"
   end
 
-  def display_battery(remote_car) do
-    # Please implement the display_battery/1 function
+  def display_battery(%RemoteControlCar{battery_percentage: 0}), do: "Battery empty"
+
+  def display_battery(%RemoteControlCar{battery_percentage: battery_percentage}),
+    do: "Battery at #{battery_percentage}%"
+
+  def drive(%RemoteControlCar{battery_percentage: 0} = remote_car), do: remote_car
+
+  def drive(%RemoteControlCar{} = remote_car) do
+    %RemoteControlCar{
+      remote_car
+      | distance_driven_in_meters: remote_car.distance_driven_in_meters + 20,
+        battery_percentage: remote_car.battery_percentage - 1
+    }
   end
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
-  end
+  # def drive(remote_car) when is_struct(remote_car, RemoteControlCar) do
+  #   %RemoteControlCar{
+  #     remote_car
+  #     | distance_driven_in_meters: remote_car.distance_driven_in_meters + 20,
+  #       battery_percentage: remote_car.battery_percentage - 1
+  #   }
+  # end
 end
