777002d2ae56db0784765e4ffce3f91a96de6950
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2025-02-16 14:27:07 +0000

    Complete "Anagram" in Elixir

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, &is_case_insensitive_anagram(base, &1))
+  end
+
+  defp is_case_insensitive_anagram(one, two) do
+    one = String.downcase(one)
+    two = String.downcase(two)
+
+    one != two && Map.equal?(build_hash_map(one), build_hash_map(two))
+  end
+
+  defp build_hash_map(word) do
+    word
+    |> String.graphemes()
+    # Alternatively: |> Enum.reduce(%{}, fn char, acc -> Map.update(acc, char, 1, fn i -> i + 1 end) end)
+    |> Enum.frequencies()
   end
 end
