ef5cab97fbbbc59d096372b08d7af4eb7715dd31
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2025-03-25 19:35:42 +0000

    Complete "Dancing Dots" in Elixir

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -4,13 +4,48 @@ defmodule DancingDots.Animation do
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: dot
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, _opts) when rem(frame_number, 4) == 0,
+    do: %{dot | opacity: dot.opacity / 2}
+
+  def handle_frame(dot, _, _), do: dot
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  @behaviour DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init(opts) do
+    velocity = Keyword.get(opts, :velocity)
+
+    case velocity do
+      velocity when is_integer(velocity) ->
+        {:ok, opts}
+
+      _ ->
+        {:error,
+         "The :velocity option is required, and its value must be a number. Got: #{inspect(velocity)}"}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    new_radius = dot.radius + (frame_number - 1) * Keyword.get(opts, :velocity)
+    %{dot | radius: new_radius}
+  end
 end
