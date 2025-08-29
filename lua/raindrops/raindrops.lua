57053296398240105d557c1b7d571807aa5defbe
Tom Hughes <4213219+tomalexhughes@users.noreply.github.com>
2025-08-29 18:04:37 +0100

    Complete "Raindrops" in Lua

--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,2 +1,10 @@
 return function(n)
+	local str = ""
+
+	if n % 3 == 0  then; str = str.."Pling" end;
+	if n % 5 == 0  then; str = str.."Plang" end;
+	if n % 7 == 0  then; str = str.."Plong" end;
+
+	if str == "" then; return tostring(n); end
+	return str
 end
